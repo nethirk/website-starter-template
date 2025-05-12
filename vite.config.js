52de81345ab4606d1ca28486267e3306b3367ed4
Rama Krishna Nethi <rnethi3@gatech.edu>
2025-05-11 20:14:24 -0400

    save changes

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,7 @@ export default defineConfig({
   root: resolve(__dirname, "src/"),
   build: {
     outDir: "../dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        home: resolve(__dirname, "src/index.html"),
-        about: resolve(__dirname, "src/about/index.html"),
-        contact: resolve(__dirname, "src/pagethree/index.html"),
-      },
+    emptyOutDir: true, 
     },
   },
 });
